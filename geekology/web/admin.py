a473ba9a785efcb9bdbadd40ebf769dcdfb8527a
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-06-07 22:36:18 +0430

    add label for use in admin account

--- geekology/web/admin.py
+++ geekology/web/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Student,Skill,StudentSkill
+from .models import Student,Skill,StudentSkill,Label
 
 # Register your models here.
 admin.site.register(Student)
 admin.site.register(Skill)
 admin.site.register(StudentSkill)
+admin.site.register(Label)
