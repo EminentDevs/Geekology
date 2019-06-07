c9c3ebada3f31be13d334088889e5d79dafb129e
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-06-07 17:16:25 +0430

    initial add student and skill in database without any error handler

--- geekology/web/views.py
+++ geekology/web/views.py
@@ -1,7 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Student,Skill,StudentSkill
 
 # Create your views here.
 
+@csrf_exempt
 def index(request):
-    return render(request,'web/index.html')
+    if request.method == 'POST':
+        if request.POST['name'] != '' and request.POST['stdno'] != '' and request.POST['phonenumber'] != '':
+            sStdno = request.POST['stdno']
+            if Student.objects.filter(studentNumber=sStdno).exists():
+                return render(request,'web/index.html')
+            else:
+                sName = request.POST['name']
+                sSexuality = request.POST['sexuality']
+                sbirthyear = request.POST['birthyear']
+                sEnterYear = request.POST['enteryear']
+                sGender = request.POST['gender']
+                sEmail = request.POST['email']
+                sSiteUrl = request.POST['siteurl']
+                sPhoneNumber = request.POST['phonenumber']
+                sTelegramId = request.POST['telegram']
+                sInstagramId = request.POST['instagram']
+                student = Student(studentName = sName,
+                                  male = sSexuality,
+                                  birthdayDate = sbirthyear,
+                                  enterYear = sEnterYear,
+                                  gender = sGender,
+                                  email = sEmail,
+                                  pWebUrl = sSiteUrl,
+                                  phoneNumber=sPhoneNumber,
+                                  telegram = sTelegramId,
+                                  instagram = sInstagramId,
+                                  studentNumber = sStdno)
+                student.save()
+                for item in Skill.objects.all():
+                    studentSkill = StudentSkill(student=student,skill=item,level=request.POST.get(item.skillName,'0'))
+                    studentSkill.save()
+                return render(request,'web/index.html')
+        else:
+            return render(request,'web/index.html')
+    else:
+        return render(request,'web/index.html')
