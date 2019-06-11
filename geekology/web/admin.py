8790babf3ea51e46328da76805246e041a1624d2
Ahmadreza Nani <ahmadr4079@gmail.com>
2019-06-11 21:04:54 +0430

    customize models view and search in admin - fix bug in sexuality value

--- geekology/web/admin.py
+++ geekology/web/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 from .models import Student,Skill,StudentSkill,Label
 
-# Register your models here.
-admin.site.register(Student)
-admin.site.register(Skill)
-admin.site.register(StudentSkill)
-admin.site.register(Label)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ['id','studentName','studentNumber','phoneNumber','email']
+    search_fields = ['studentName','studentNumber']
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ['id','skillName','categorylabel']
+    search_fields = ['skillName']
+
+@admin.register(StudentSkill)
+class StudentSkillAdmin(admin.ModelAdmin):
+    list_display = ['id','student','skill','level']
+    list_filter = ['level']
+
+@admin.register(Label)
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ['id','labelName']
+    search_fields = ['labelName']
